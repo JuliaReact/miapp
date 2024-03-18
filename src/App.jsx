975b25b2c22210d7e44b58527d3532156c07d774
JuliaReact <luniclara@gmail.com>
2024-03-18 18:32:51 +0100

    cambios nuevos

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
+import { useState } from 'react';
+import React from 'react'
 import './App.css';
 
+
+
 function App() {
+  const [nombre, setnombre] = useState('hola universo')
+  
   const saludar= () =>{
-    let nombre= "cuchi"
-    return nombre;
-  };
+    setnombre("cuchi") 
+  
+  }
   return (
     <div >
       <h1>Mi app</h1>
-      <p>Hola {} </p>
-
+      <p> {nombre} </p>
+      <button type='button' onClick={() => saludar()}>cambiar saludo</button>
+     
     </div>
   );
 }
