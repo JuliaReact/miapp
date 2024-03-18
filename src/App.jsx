2be899202f69fa06790335948cecd439541ff528
JuliaReact <luniclara@gmail.com>
2024-03-18 17:06:32 +0100

    cambios

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  const saludar= () =>{
+    let nombre= "cuchi"
+    return nombre;
+  };
+  return (
+    <div >
+      <h1>Mi app</h1>
+      <p>Hola {} </p>
+
+    </div>
+  );
+}
+
+export default App;
