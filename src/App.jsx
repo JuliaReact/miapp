fec0eef8e2429d00c5c9a521d0ed1c5d40563d2a
JuliaReact <luniclara@gmail.com>
2024-04-26 18:32:17 +0200

    commit por cambio de rama

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
 import { useState } from "react";
 import "./App.css";
-import Formulario from "./componentes/formulario";
+//import Formulario from "./componentes/formulario";
+//import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   const [titulo, setTitulo] = useState("La pagina de Cuchi");
 
-  return (
-    <div className="superGeneral">
-      <Formulario miTitulo={titulo} />
-    </div>
-  );
+  return <div className="superGeneral"></div>;
 }
 
 export default App;
