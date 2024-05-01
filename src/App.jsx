5f2e5c85e6b9f9b35f716f9a8ba0b67301cdc3fc
JuliaReact <luniclara@gmail.com>
2024-05-01 13:47:31 +0200

    enrutar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { useState } from "react";
 import "./App.css";
 //import Formulario from "./componentes/formulario";
-//import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   const [titulo, setTitulo] = useState("La pagina de Cuchi");
 
-  return <div className="superGeneral"></div>;
+  return (
+    <div className="App">
+      <Routes></Routes>
+    </div>
+  );
 }
 
 export default App;
