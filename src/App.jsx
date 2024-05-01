a8a6760629f6c33f696109cbbeb1836e72282c79
JuliaReact <luniclara@gmail.com>
2024-05-01 14:21:00 +0200

    creando rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
 import { useState } from "react";
 import "./App.css";
-//import Formulario from "./componentes/formulario";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Formulario from "./componentes/formulario";
+import { BrowserRouter, Route, Routes, Switch } from "react-router-dom";
+//import Pintanombre from "./componentes/pintanombre";
+import Saludar from "./componentes/saludar";
 
 function App() {
   const [titulo, setTitulo] = useState("La pagina de Cuchi");
 
   return (
     <div className="App">
-      <Routes></Routes>
+      <Routes>
+        <Switch>
+          <Route exact patch="/formulario" component={Formulario} />
+          <Route exact patch="/saludar" component={Saludar} />
+        </Switch>
+      </Routes>
     </div>
   );
 }
